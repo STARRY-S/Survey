1bd60802c8629bf04b12ca4786b3984d71f7e6af
STARRY-S <hx916584160@gmail.com>
2021-06-16 21:28:35 +0800

    Updated question submit.

--- routes/submit.js
+++ routes/submit.js
@@ -13,11 +13,33 @@ router.post('/', (req, res) => {
     return;
   }
 
-  // console.log(req.body);
-  for (const obj in req.body) {
-    console.log(req.body[obj]);
+  // console.log(req.session.obj_list);
+  let obj_list = req.session.obj_list;
+  for (let i = 1; i < obj_list.length; ++i) {
+    // console.log(obj_list[i]);
+    switch (obj_list[i].type) {
+      case 'input':
+        obj_list[i].answer = req.body[`question_${i}`];
+        // console.log(req.body[`question_${i}`]);
+        break;
+      case 'select':
+        obj_list[i].answer = req.body[`question_${i}`];
+        // console.log(req.body[`question_${i}`]);
+        break;
+      case 'multiselect':
+        obj_list[i].answer = "";
+        for (let j = 1; j <= obj_list[i].q_num; ++j) {
+          if (req.body[`cb_${i}_${j}`]) {
+            obj_list[i].answer += obj_list[i].q_list[j-1] + " ";
+          }
+        }
+        break;
+      default:
+        break;
+    }
   }
-  res.json(req.body);
+  console.log(obj_list);
+  res.json(obj_list);
 });
 
 module.exports = router;
