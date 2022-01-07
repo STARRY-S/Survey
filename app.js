9daecafbd9f30237a7e8dfd4704c731fab20a1af
STARRY-S <hxstarrys@gmail.com>
2022-01-08 02:01:41 +0800

    Update URL Check.

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
 
     if (url_check && host !== url && host !== `${url}:${port}`) {
         let protocol = (utils.getConfig().ssl.enable) ? "https" : "http";
-        res.redirect(`${protocol}://${url}:${port}`);
+        res.redirect(`${protocol}://${url}`);
         res.end();
         return;
     }
